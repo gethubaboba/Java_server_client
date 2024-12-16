aca3bd46939bfd447b19522470548db0ab6bf3e0
gethubaboba <106423848+gethubaboba@users.noreply.github.com>
2024-12-16 07:57:27 +0300

    Update client.java
    da

--- client.java
+++ client.java
@@ -1,19 +1,17 @@
 package java_server;
-import java_server.*;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 public class client_server {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String ip_adress = "25.57.231.32";
         try {
-            Socket soc = new Socket("10.150.3.60", 1000);
-            Socket soc_log = new Socket("10.150.3.60", 1001);
-            Socket soc_log1 = new Socket("10.150.3.60", 1001);
+            Socket soc = new Socket(ip_adress, 1000);
+            Socket soc_log = new Socket(ip_adress, 1001);
 
             ObjectOutputStream writer = new ObjectOutputStream(soc.getOutputStream());
             ObjectOutputStream writer_log = new ObjectOutputStream(soc_log.getOutputStream());
-            ObjectOutputStream writer_log1 = new ObjectOutputStream(soc_log1.getOutputStream());
             InputStream reader = soc.getInputStream();
             ObjectInputStream reader_log = new ObjectInputStream(soc_log.getInputStream());
 
@@ -26,7 +24,7 @@ public class client_server {
 
             soc.close();
 
-            soc = new Socket("10.150.3.60", 1000);
+            soc = new Socket(ip_adress, 1000);
             ObjectOutputStream writer11 = new ObjectOutputStream(soc.getOutputStream());
             InputStream reader1 = soc.getInputStream();
 
@@ -35,14 +33,15 @@ public class client_server {
             writer11.writeObject(UserName1);
             System.out.println(reader1.read() == 1 ? "User registred suc cock":"User already exist");
 
-
-
             System.out.println("INput Login");
             String UserLogin = in.nextLine();
 
             writer_log.writeObject(UserLogin);
+            System.out.println(reader_log.available());
             System.out.println(reader_log.read() == 1 ? "User logined suc cock":"UserLogin already exist");
 
+            Socket soc_log1 = new Socket(ip_adress, 1001);
+            ObjectOutputStream writer_log1 = new ObjectOutputStream(soc_log1.getOutputStream());
             System.out.println("INput Login");
             String UserLogin1 = in.nextLine();
 
@@ -50,8 +49,9 @@ public class client_server {
 
             writer_log1.writeObject(UserLogin1);
             System.out.println(reader_log1.read() == 1 ? "User logined suc cock":"UserLogin already exist");
-
-            EmailFile efile = new EmailFile(new File("C:\\Users\\fpm.shibitov\\Desktop\\test_file.txt"));
+            String file_path1 = "fpm.shibitov";
+            String file_path2 = "ADM";
+            EmailFile efile = new EmailFile(new File("C:\\Users\\" + file_path2 + "\\Desktop\\test_file.txt"));
             System.out.println("write sender");
             String mes_sender = in.next();
             System.out.println("write resiver");
@@ -65,10 +65,11 @@ public class client_server {
             writer_log.writeObject(messege);
 
 
-
             Message m = (Message) reader_log1.readObject();
 
-            System.out.println(m.message);
+            System.out.println(mes_sender + " -> " + mes_resiver + " messege: " +m.message);
+
+            m.file.getFile(new File("C:\\Users\\" + file_path2 + "\\Desktop\\sendet_file)"));
 
             soc.close();
             soc_log.close();
