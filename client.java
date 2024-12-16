6ce797ed782684dd679dc3df17877c3b424c207e
gethubaboba <106423848+gethubaboba@users.noreply.github.com>
2024-12-16 09:39:12 +0300

    Update client.java
    add RMI

--- client.java
+++ client.java
@@ -1,14 +1,21 @@
 package java_server;
 import java.io.*;
 import java.net.*;
+import java.net.UnknownHostException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.rmi.*;
 public class client_server {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String ip_adress = "25.57.231.32";
+        String ip_adress = "10.150.3.228";
         try {
             Socket soc = new Socket(ip_adress, 1000);
             Socket soc_log = new Socket(ip_adress, 1001);
+
+            Registry reg = LocateRegistry.getRegistry(ip_adress);
+            LoginedUsers zaglushka = (LoginedUsers) reg.lookup("LoginedUsers");
 
             ObjectOutputStream writer = new ObjectOutputStream(soc.getOutputStream());
             ObjectOutputStream writer_log = new ObjectOutputStream(soc_log.getOutputStream());
@@ -49,14 +56,24 @@ public class client_server {
 
             writer_log1.writeObject(UserLogin1);
             System.out.println(reader_log1.read() == 1 ? "User logined suc cock":"UserLogin already exist");
+
+            String[] lu = zaglushka.getLoginedUsers();
+
             String file_path1 = "fpm.shibitov";
             String file_path2 = "ADM";
-            EmailFile efile = new EmailFile(new File("C:\\Users\\" + file_path2 + "\\Desktop\\test_file.txt"));
+            EmailFile efile = new EmailFile(new File("C:\\Users\\" + file_path1 + "\\Desktop\\test_file.txt"));
+
             System.out.println("write sender");
             String mes_sender = in.next();
-            System.out.println("write resiver");
-            String mes_resiver = in.next();
+
+            for(int i = 0; i < lu.length; i++){
+                System.out.println(i+1 + " " + lu[i]);
+            }
+            System.out.println("select resiver " + "(1-first " + lu.length + "-last)");
+            int SelectResiver = in.nextInt();
+            String mes_resiver = lu[SelectResiver-1];
             in.nextLine();
+
             System.out.println("write messege");
             String mess = in.nextLine();
 
@@ -69,7 +86,7 @@ public class client_server {
 
             System.out.println(mes_sender + " -> " + mes_resiver + " messege: " +m.message);
 
-            m.file.getFile(new File("C:\\Users\\" + file_path2 + "\\Desktop\\sendet_file)"));
+            m.file.getFile(new File("C:\\Users\\" + file_path1 + "\\Desktop\\sendet_file)"));
 
             soc.close();
             soc_log.close();
@@ -80,6 +97,8 @@ public class client_server {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (NotBoundException e) {
             throw new RuntimeException(e);
         }
     }
